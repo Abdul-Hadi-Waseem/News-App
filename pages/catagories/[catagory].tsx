c6a632978c50f90dd0d9c0d6bba69dbecd1ade8e
Abdul Hadi Waseem <shaikhahw54@gmail.com>
2023-04-30 04:16:05 +0500

    Updated [catagory].tsx

--- pages/catagories/[catagory].tsx
+++ pages/catagories/[catagory].tsx
@@ -1,15 +1,18 @@
+
 import NewsArticlesGrid from '@/components/NewsArticlesGrid'
 import { NewsArticle, NewsResponse } from '@/models/NewsArticle'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import React from 'react'
-import { Alert } from 'react-bootstrap'
-interface CatagoryNewsPageProps{
-    newsArticles:NewsArticle[]
+
+
+interface CatagoryNewsPageProps {
+    newsArticles: NewsArticle[]
 }
-export const getStaticPaths:GetStaticPaths=async()=>{
-    const catagorySlugs=[
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const catagorySlugs = [
         "business",
         "entertainment",
         "general",
@@ -18,50 +21,48 @@ export const getStaticPaths:GetStaticPaths=async()=>{
         "sports",
         "technology"
     ];
-    const paths = catagorySlugs.map((slug)=>(
-        {
-            params:{catagory:slug}
-        }
-    ))
+    const paths = catagorySlugs.map((slug) => ({
+        params: { catagory: slug }
+    }))
     return {
         paths,
-        fallback:false
+        fallback: false
     }
 
 }
-export const getStaticProps:GetStaticProps<CatagoryNewsPageProps>=async({params})=>{
-    const catagory=params?.catagory?.toString();
-    const res=await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${catagory}&apiKey=${process.env.NEWS_API_KEY}`)
-    const newsResponse:NewsResponse = await res.json();
-    if(!newsResponse){
-        return{
-            notFound:true
+
+export const getStaticProps: GetStaticProps<CatagoryNewsPageProps> = async ({ params }) => {
+    const catagory = params?.catagory?.toString();
+    const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${catagory}&apiKey=${process.env.NEWS_API_KEY}`)
+    const newsResponse: NewsResponse = await res.json();
+    if (!newsResponse || !newsResponse.articles) {  // Check if newsResponse.articles is defined
+        return {
+            notFound: true
         }
     }
     return {
-        props:{
-            newsArticles : JSON.parse(JSON.stringify(newsResponse.articles ))
+        props: {
+            newsArticles: newsResponse.articles
         },
-        revalidate: 5*60,
+        revalidate: 5 * 60,
     }
 }
-export default function CatagoryNewsPage({newsArticles}:CatagoryNewsPageProps) {
-  const router =useRouter();
-  const catagoryName=router.query.catagory?.toString();
-  const title = "Catagory : "+ catagoryName?.slice(0,1).toUpperCase() + catagoryName?.slice(1)
+
+export default function CatagoryNewsPage({ newsArticles }: CatagoryNewsPageProps) {
+    const router = useRouter();
+    const catagoryName = router.query.catagory?.toString();
+    const title = catagoryName ? "Category : " + catagoryName?.slice(0, 1).toUpperCase() + catagoryName?.slice(1) : "Category Not Found";
     return (
-    <>
-    <Head>
-        <title key={"title"}>{`${title} -NextJS News App`}</title>
-    </Head>
-      <main>
-        <h1>{title}</h1>
-        <Alert>
-                    This is page uses <strong>getStaticProps</strong> for very high page loading speed
-                    and <strong>incremental static regeneration</strong> to show data not older than <strong>5 minutes</strong>.
-                </Alert>
-        <NewsArticlesGrid articles={newsArticles}/>
-      </main>
-    </>
-  )
+        <>
+            <Head>
+                <title key={"title"}>{`${title} -NextJS News App`}</title>
+            </Head>
+            <main>
+                <h1>{title}</h1>
+              
+                <NewsArticlesGrid articles={newsArticles} />
+            </main>
+        </>
+    )
 }
+
